7c8ea345fd97755e7eff648527fff287c47dc786
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2023-11-08 03:10:27 +0600

    Create GetFile Controller

---
+++ CityInfo.API/Controllers/FilesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesController:ControllerBase
+    {
+        private readonly FileExtensionContentTypeProvider _extensionContentTypeProvider;
+
+        public FilesController(FileExtensionContentTypeProvider extensionContentTypeProvider)
+        {
+            _extensionContentTypeProvider = extensionContentTypeProvider ?? throw new System.ArgumentNullException(nameof(extensionContentTypeProvider));
+        }
+
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(string fileId) 
+        {
+            var pathToFile = "getting-started-with-rest-slides.pdf";
+
+            if (!System.IO.File.Exists(pathToFile))
+            {
+                return NotFound();
+            }
+
+            if(!_extensionContentTypeProvider.TryGetContentType(pathToFile, out var contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+            return File(bytes, contentType, Path.GetFileName(pathToFile));
+
+
+
+        }
+    }
+}
