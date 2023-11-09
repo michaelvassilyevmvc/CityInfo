ff6b39644a72be432ace7ab9685639911a8015aa
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2023-11-09 10:19:28 +0600

    Добавление EF с базой данных Sqlite

---
+++ CityInfo.API/DbContexts/CitiInfoContext.cs
@@ -0,0 +1,22 @@
+﻿using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.DbContexts
+{
+    public class CitiInfoContext: DbContext
+    {
+        public DbSet<City> Cities { get; set; } = null!;
+        public DbSet<PointOfInterest> PointOfInterests { get; set; } = null!;
+
+        public CitiInfoContext(DbContextOptions<CitiInfoContext> options):base(options)
+        {
+            
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlite("connectionstring");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+    }
+}
